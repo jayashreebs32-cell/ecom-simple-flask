b08d1c48d86873af699024d47216ab7e84375ad2
Jayashree BS <jayajayabs05@gmail.com>
2026-02-04 22:18:54 +0530

    change port

--- app.py
+++ app.py
@@ -288,6 +288,6 @@ def orders_history():
 if __name__ == "__main__":
     print("=" * 60)
     print("Flask app starting...")
-    print("Open http://localhost:8000 in your browser")
+    print("Open http://localhost:5000 in your browser")
     print("=" * 60)
     app.run(host="127.0.0.1", port=5000, debug=True)
